d4a1ac92bd45a73d3d650001f1e08e58362692fb
Manbo1996 <manbo1996@naver.com>
2025-06-04 18:20:55 +0900

    250604) Notice 관련 작업(전영산)
    [수정한 부분]
    NoticeService
    NoticeServiceimpl
    NoticeDao

--- SOYO_Project/src/main/java/com/kh/soyo/notice/model/service/NoticeService.java
+++ SOYO_Project/src/main/java/com/kh/soyo/notice/model/service/NoticeService.java
@@ -1,5 +1,19 @@
 package com.kh.soyo.notice.model.service;
 
-public interface NoticeService {
+import java.util.ArrayList;
+import com.kh.soyo.common.model.vo.PageInfo;
+import com.kh.soyo.notice.model.vo.Notice;
 
+public interface NoticeService {
+	// 공지 목록
+	ArrayList<Notice> noticeList(PageInfo pi);
+	
+	// 공지 상세
+	Notice noticeDetail(int noticeNo);
+		
+	// 공지 수정
+	int updateNotice(int noticeNo);
+
+	// 공지 삭제
+	int deleteNotice(int noticeNo);
 }
