ec904d72fa53e2b40a8ccb1e909dd37f23cfe228
Manbo1996 <manbo1996@naver.com>
2025-06-04 16:24:16 +0900

    250604) Notice.vo 작업(전영산)
    제곧내

--- SOYO_Project/src/main/java/com/kh/soyo/notice/model/vo/Notice.java
+++ SOYO_Project/src/main/java/com/kh/soyo/notice/model/vo/Notice.java
@@ -1,5 +1,21 @@
 package com.kh.soyo.notice.model.vo;
 
-public class Notice {
+import java.sql.Date;
+import org.apache.ibatis.type.Alias;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Alias("notice")
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Notice {
+	private int noticeNo;			// 공지 번호
+	private String noticeTitle;		// 공지 제목
+	private String noticeContent;	// 공지 내용
+	private Date createDate;		// 공지 작성일
+	private String status;			// 공지 삭제 여부
 }
