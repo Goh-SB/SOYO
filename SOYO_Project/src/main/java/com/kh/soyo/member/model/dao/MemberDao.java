4bc8c8cdf3159dc146a1b253c02ba4d015832a80
Beom <ssbb88088@gmail.com>
2025-06-09 10:50:41 +0900

    250609) 클래스 경로 오류 수정 - 승범
    MemberDao 클래스와 BoardDao 클래스 경로가 서로 바뀌어있었음.

--- SOYO_Project/src/main/java/com/kh/soyo/member/model/dao/MemberDao.java
+++ SOYO_Project/src/main/java/com/kh/soyo/member/model/dao/MemberDao.java
@@ -1,8 +1,23 @@
 package com.kh.soyo.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.soyo.member.model.vo.Member;
 
 @Repository
 public class MemberDao {
+
+	public Member loginMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.selectOne("memberMapper.loginMember", m);
+		
+	}
+
+	// 회원 정보 수정용 메소드
+	public int updateMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.update("memberMapper.updateMember", m);
+	}
 
 }
